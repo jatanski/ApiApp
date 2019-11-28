c6d62cc53aa90e70f02d0f845a523bac979c1435
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-11-28 23:37:11 +0100

    fetch all supported crypto and real currencies

--- src/js/models/model.js
+++ src/js/models/model.js
@@ -2,84 +2,105 @@ class BaseModel {
     constructor() {}
 
     async fetchAllCoinNames() {
+        const currencies = [];
+
+        //fetch coins from api listing nearly all supported crypto
         const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist");
         const data = await response.json();
-        if (data.Response !== "Success") {
+        if (!!data && data.Response !== "Success") {
+            console.error("failed to fetch from main source");
+            console.log(data);
             return [];
         }
-
-        const currencies = [];
         Object.values(data.Data).map(coin => {
             currencies.push({ name: coin.CoinName, symbol: coin.Name });
         });
-        console.log(currencies);
 
-        const additionalCurrencies = await this.fetchCurrencies();
+        //fetch real currencies
+        const realCurrencies = await this.fetchRealCurrencies();
+        Object.values(realCurrencies).map(currency => {
+            currencies.push({ name: currency.name, symbol: currency.code });
+        });
 
-        additionalCurrencies.forEach(currency => {
+        //fetch some more supported crypto
+        const possiblyNewCurrencies = await this.fetchAdditionalCurrencies();
+        const additionalCurrencies = [];
+        possiblyNewCurrencies.forEach(currency => {
             let exists = currencies.find(el => {
-                // console.log(el.symbol, currency);
                 return el.symbol === currency;
             });
             if (!exists) {
-                currencies.push({ name: currency, symbol: currency });
+                additionalCurrencies.push({ name: currency, symbol: currency });
             }
         });
+        currencies.push(...additionalCurrencies);
 
         return currencies;
     }
 
-    async fetchPrice(symbol1, symbol2) {
+    async fetchPrice(symbol1, symbol2, supressErrors = false) {
         const responseJSON = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol1}&tsyms=${symbol2}`);
         const response = await responseJSON.json();
         if (!!response.Response) {
-            console.error("Couldn't fetch price");
+            if (!supressErrors) {
+                console.error("Couldn't fetch price");
+            }
             return -1;
         } else {
             return response[symbol2];
         }
     }
 
-    async fetchCurrencies() {
+    async fetchAdditionalCurrencies() {
         const responseJSON = await fetch(`https://min-api.cryptocompare.com/data/v2/cccagg/pairs`);
         const response = await responseJSON.json();
         const currencies = [];
-        if (false) {
-            console.error("Couldn't fetch price");
-            return -1;
-        } else {
-            console.log(response.Data.pairs);
 
+        if (!response.Data.pairs) {
+            console.error("Couldn't fetch prices of additional currencies");
+            console.log(response);
+            return [];
+        } else {
             Object.keys(response.Data.pairs).map(currency => {
                 currencies.push(currency);
             });
-            console.log(currencies);
         }
+
         return currencies;
     }
 
-    // async checkCurrencies(currencies) {
-    //     console.log(currencies.length);
-    //     for (let i = 0; i < currencies.length; i++) {
-    //         let el = currencies[i].symbol;
-    //         let exists = await this.checkIfExsists(el);
-    //         if (!exists) {
-    //             // console.log(el);
-    //         } else {
-    //             // console.log(el);
-    //         }
-    //     }
-    // }
+    async fetchRealCurrencies() {
+        const possibleRealCurrencies = [];
+        const response = await fetch(
+            `https://gist.githubusercontent.com/Fluidbyte/2973986/raw/255641f9d09bc4973c139b927c0d602106d227ae/Common-Currency.json`
+        );
+        const currencies = await response.json();
+        Object.values(currencies).map(currency => {
+            possibleRealCurrencies.push({ name: currency.name, symbol: currency.code });
+        });
+        const existingCurrencies = await this.checkCurrencies(possibleRealCurrencies);
 
-    // async checkIfExsists(currency) {
-    //     const response = await this.fetchPrice(currency, "USD");
-    //     if (response === -1) {
-    //         console.log(currency + " doesnt exist");
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-    // }
+        return existingCurrencies;
+    }
+
+    //checks if currency is supported by api
+    async checkCurrencies(currencies) {
+        const existingCurrencies = [];
+        for (let i = 0; i < currencies.length; i++) {
+            let el = currencies[i];
+            let exists = await this.checkIfExsists(el.symbol);
+            if (exists) {
+                existingCurrencies.push(el);
+            }
+        }
+
+        return existingCurrencies;
+    }
+
+    async checkIfExsists(currency) {
+        const response = await this.fetchPrice(currency, "USD", true);
+        return response !== -1;
+    }
 }
 
 export default BaseModel;
