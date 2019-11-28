1b7b06d7052268c3f82ad5ecc8556c3f8118b307
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-11-28 22:20:19 +0100

    at this point alternative method of fetching currencies kinda exists

--- src/js/models/model.js
+++ src/js/models/model.js
@@ -1,5 +1,8 @@
 class BaseModel {
-    constructor() {}
+    constructor() {
+        this.fetchCurrencies();
+        this.fetchSomething();
+    }
 
     async fetchAllCoinNames() {
         const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist");
@@ -9,7 +12,7 @@ class BaseModel {
         }
 
         const currencies = [];
-        Object.values(data.Data).map((coin, index) => {
+        Object.values(data.Data).map(coin => {
             currencies.push({ name: coin.CoinName, symbol: coin.Name });
         });
         console.log(currencies);
@@ -17,10 +20,69 @@ class BaseModel {
     }
 
     async fetchPrice(symbol1, symbol2) {
-        const response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol1}&tsyms=${symbol2}`);
-        const currency = await response.json();
-        console.log(currency);
-        return currency;
+        const responseJSON = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol1}&tsyms=${symbol2}`);
+        const response = await responseJSON.json();
+        if (!!response.Response) {
+            console.error("Couldn't fetch price");
+            return -1;
+        } else {
+            return response[symbol2];
+        }
+    }
+
+    async fetchCurrencies() {
+        const response = await fetch(
+            `https://gist.githubusercontent.com/Fluidbyte/2973986/raw/255641f9d09bc4973c139b927c0d602106d227ae/Common-Currency.json`
+        );
+        const currencies = await response.json();
+        console.log(currencies);
+
+        const arr = [];
+        Object.values(currencies).map(currency => {
+            arr.push({ name: currency.name, symbol: currency.code });
+        });
+        console.log(arr);
+        // await this.checkCurrencies(arr);
+        return currencies;
+    }
+
+    async fetchSomething() {
+        const responseJSON = await fetch(`https://min-api.cryptocompare.com/data/v2/cccagg/pairs`);
+        const response = await responseJSON.json();
+        if (false) {
+            console.error("Couldn't fetch price");
+            return -1;
+        } else {
+            console.log(response.Data.pairs);
+            const arr = [];
+            Object.keys(response.Data.pairs).map(currency => {
+                arr.push(currency);
+            });
+            console.log(arr);
+        }
+    }
+
+    async checkCurrencies(currencies) {
+        console.log(currencies.length);
+        for (let i = 0; i < currencies.length; i++) {
+            let el = currencies[i].symbol;
+            let exists = await this.checkIfExsists(el);
+            if (!exists) {
+                // console.log(el);
+            } else {
+                // console.log(el);
+            }
+        }
+    }
+
+    async checkIfExsists(currency) {
+        const response = await this.fetchPrice(currency, "USD");
+        if (response === -1) {
+            console.log(currency + " doesnt exist");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
 
